148b97e692abd946ebbf6ff4a950684257e0ff79
FedesConsultora <info@fedesconsultora.com>
2026-08-13 10:45:08 -0300

    style(admin): include modal theme layer

--- src/pages/Admin/AdminDashboardTheme.jsx
+++ src/pages/Admin/AdminDashboardTheme.jsx
@@ -1,6 +1,7 @@
 import AdminDashboard from './AdminDashboard'
 import './AdminTheme.scss'
 import './AdminTableTheme.scss'
+import './AdminOverlayTheme.scss'
 
 export default function AdminDashboardTheme() {
   return <AdminDashboard />
