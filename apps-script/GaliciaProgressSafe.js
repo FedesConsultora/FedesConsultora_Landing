3587210db35033b54e3783541946349da08cd769
Enzo Pinotti <enzopinottii@gmail.com>
2026-08-27 14:03:09 -0300

    Actualiza sesion y atribucion durante el progreso

--- apps-script/GaliciaProgressSafe.js
+++ apps-script/GaliciaProgressSafe.js
@@ -1,7 +1,11 @@
 function saveGaliciaProgressSafe_(data) {
-  var result=saveGaliciaProgress_(data||{});
+  data=data||{};
+  var result=saveGaliciaProgress_(data);
   var lead=resolveGaliciaLead_(safeString_(data&&data.leadId));
   if (!lead || safeString_(lead.status)==='complete') return result;
+
+  touchGaliciaLeadAttribution_(data,lead.lead_id);
+  lead=resolveGaliciaLead_(lead.lead_id)||lead;
 
   var highest=highestAnsweredQuestion_(getLeadAnswersMap_(lead.lead_id));
   if (highest && highest!==safeString_(lead.last_question_key)) {
